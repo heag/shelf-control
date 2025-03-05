e4a2c5aef883a15cc6d907f6c944b1e2f983571b
Hector <39012199+heag@users.noreply.github.com>
2025-03-05 14:35:52 +0100

    HEC-007 add pie card (#8)
    Co-authored-by: AzureAD\HectorGarcia <hgarcia@globalshares.com>

--- src/app/global/laboratory/card/card-laboratory.component.ts
+++ src/app/global/laboratory/card/card-laboratory.component.ts
@@ -1,13 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { PieChartComponent } from '../../components/pie-chart/pie-chart.component';
+import { ChartPieData } from '../../components/pie-chart/models/pie-chart-data';
 
 @Component({
   selector: 'hec-card-laboratory',
-  imports: [CardComponent],
+  imports: [CardComponent, PieChartComponent],
   templateUrl: './card-laboratory.component.html',
   styleUrl: './card-laboratory.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardLaboratoryComponent {
-
+  chartData: ChartPieData[] = [
+    {
+      label: 'FrontEnd',
+      value: 55,
+      order: 1,
+    },
+    {
+      label: 'Backend',
+      value: 35,
+      order: 2,
+    },
+    {
+      label: 'Devops',
+      value: 10,
+      order: 3,
+    },
+  ];
 }
